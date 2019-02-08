2162c584618212977817cf84bb4ecde5b264bc0d
Luis Elementary <lr88090@outlook.com>
2019-02-07 22:17:42 -0600

    the routes are added

--- index.js
+++ index.js
@@ -12,9 +12,21 @@ http.createServer((req,res) =>{
 const express = require('express');
 const app = express();
 
+
+//rutas
+    // el simbolo / es la ruta inicial
 app.get('/',(req,res) =>{//get es el metodo de peticion (request,respuesta)
     res.end('Hello world');
 });
+
+app.get('/login', (req,res) =>{
+    res.end('aqui va el login');
+});
+
+app.get('*' ,(req,res) =>{
+    res.end('Archivo no encontrado');
+});
+
 app.listen(3000, () =>{
     console.log('Server active');
 });
